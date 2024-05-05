92a7cfc7d9ec6b15cf5a117bb3467452c0bb04be
abdul mustaan <abdulmustaan941@outlook.com>
2024-05-05 14:46:21 +0530

    added dtos

--- WebAPI/WebAPI/Controllers/CityController.cs
+++ WebAPI/WebAPI/Controllers/CityController.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebAPI.Dtos;
 using WebAPI.Interfaces;
 using WebAPI.Models;
 
@@ -9,39 +12,49 @@ namespace WebAPI.Controllers
   [ApiController]
   public class CityController : ControllerBase
   {
-    private readonly ICityRepository repo;
-    public CityController(ICityRepository repo)
+    private readonly IUnitOfWork uow;
+    public CityController(IUnitOfWork uow)
     {
-      this.repo = repo;
+      this.uow = uow;
     }
-
     // GET api/city
     [HttpGet]
-    [HttpGet("api/City")]
     public async Task<IActionResult> GetCities()
     {
-      var cities = await repo.GetCitiesAsync();
-      return Ok(cities);
+      var cities = await uow.CityRepository.GetCitiesAsync();
+
+      var citiesDto = from c in cities
+                      select new CityDto()
+                      {
+                        Id = c.Id,
+                        Name = c.Name
+                      };
+
+      return Ok(citiesDto);
     }
 
     // Post api/city/post --Post the data in JSON Format
     [HttpPost("post")]
-    public async Task<IActionResult> AddCity(City city)
+    public async Task<IActionResult> AddCity(CityDto cityDto)
     {
-      repo.AddCity(city);
-      await repo.SaveAsync();
+      var city = new City
+      {
+        Name = cityDto.Name,
+        LastUpdatedBy = 1,
+        LastUpdatedOn = DateTime.Now
+      };
+
+      uow.CityRepository.AddCity(city);
+      await uow.SaveAsync();
       return StatusCode(201);
     }
-
-
     [HttpDelete("delete/{id}")]
     public async Task<IActionResult> DeleteCity(int id)
     {
-      repo.DeleteCity(id);
-      await repo.SaveAsync();
+      uow.CityRepository.DeleteCity(id);
+      await uow.SaveAsync();
       return Ok(id);
     }
-
     // Post api/city/add?cityname=Miami
     // Post api/city/add/Los Angeles
     // [HttpPost("add")]
