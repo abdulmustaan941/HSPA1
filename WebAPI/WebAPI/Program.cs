638e94cde0ee88f9d43623274b0f631443ec3334
abdul mustaan <abdulmustaan941@outlook.com>
2024-05-07 15:52:26 +0530

    handling errors globally using middleware

--- WebAPI/WebAPI/Program.cs
+++ WebAPI/WebAPI/Program.cs
@@ -5,35 +5,43 @@ using WebAPI.Helpers;
 using WebAPI.Data;
 using WebAPI.Data.Repo;
 using WebAPI.Interfaces;
+using WebAPI.Extensions;
+using ServiceStack.Text;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddDbContext<DataContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnnection")));
-builder.Services.AddControllers();  
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
-//builder.Services.AddScoped<ICityRepository, CityRepository>();
-//builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-builder.Services.AddCors();
-//builder.Services.AddControllers().AddNewtonsoftJson();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-  app.UseSwagger();
-  app.UseSwaggerUI();
+  private static void Main(string[] args)
+  {
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+    builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnnection")));
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
+    //builder.Services.AddScoped<ICityRepository, CityRepository>();
+    //builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+    builder.Services.AddCors();
+    //builder.Services.AddControllers().AddNewtonsoftJson();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+      app.UseSwagger();
+      app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+    app.UseCors(m => m.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+    app.MapControllers();
+    //app.ConfigureExceptionHandler(env);
+    app.Run();
+  }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-app.UseCors(m => m.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
-app.MapControllers();
-
-app.Run();
